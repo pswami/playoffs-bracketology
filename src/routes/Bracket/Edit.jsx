711135156984c5fdb5c58e466f49b2ce8159a4a4
Pauras Swami <pauras53@yahoo.com>
2018-03-25 19:48:25 -0700

    use icons and radio

--- src/routes/Bracket/Edit.jsx
+++ src/routes/Bracket/Edit.jsx
@@ -2,16 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-import data from '../../mock.json';
+import data from '../../data/mock.json';
+import teams from '../../data/teams.json';
 
-console.log(data);
+import './style.scss';
+
+const iconLink = tricode => `http://i.cdn.turner.com/nba/nba/assets/logos/teams/secondary/web/${tricode}.svg`
+
 const TeamOption = ({ series }) => (
-  <div className={cx('form-row mx-auto w-50', { 'disabled': !series.isScheduleAvailable })}>
-    <select id="team" className="form-control col-md-7">
-      <option value={series.bottomRow.teamId}>{series.bottomRow.teamId}</option>
-      <option value={series.topRow.teamId}>{series.topRow.teamId}</option>
-    </select>
-    <span> in </span>
+  <div className={cx('form-group form-row flex align-items-center', { 'disabled': !series.isScheduleAvailable })}>
+    <fieldset>
+      <div className="form-check form-check-inline">
+        <label className="form-check-label  team-option">
+          <input
+            type="radio"
+            className="form-check-input"
+            name={`series-${series.seriesId}`}
+            value={teams[series.bottomRow.teamId].tricode}
+            hidden
+          />
+          <img className="icon" src={iconLink(teams[series.bottomRow.teamId].tricode)} />
+        </label>
+      </div>
+      <div className="form-check form-check-inline">
+        <label className="form-check-label  team-option">
+          <input
+            type="radio"
+            className="form-check-input"
+            name={`series-${series.seriesId}`}
+            value={teams[series.topRow.teamId].tricode}
+            hidden
+          />
+          <img className="icon" src={iconLink(teams[series.topRow.teamId].tricode)} />
+        </label>
+      </div>
+    </fieldset>
+    <span> in <br/></span>
     <select id="games" className="form-control col-md-4">
       <option value={4}>4</option>
       <option value={5}>5</option>
