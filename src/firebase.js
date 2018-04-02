d1b7474360ca98e5077dd37c982654284b1bac38
Pauras Swami <pauras53@yahoo.com>
2018-04-01 17:49:18 -0700

    create structure

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 var config = {
   apiKey: "AIzaSyD4GPFeB4Q4bwN8PHUrQosqn4GbBgFV1cc",
@@ -12,6 +13,43 @@ var config = {
 firebase.initializeApp(config);
 
 export default firebase;
-
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
+export const db = firebase.database();
+export const firestore = firebase.firestore();
+
+// Refs
+
+export const createMatchups = ({ uid, groupId, matchups }) => {
+  const matchupRef = firestore.collection("matchup");
+  const batch = firestore.batch();
+
+  matchups.forEach(matchup => {
+    const doc = matchupRef.doc();
+
+    batch.set(doc, {
+      seriesId: matchup.seriesId,
+      team: matchup.team,
+      winIn: matchup.winIn,
+      uid: uid,
+      groupId: 4,
+    });
+
+    return doc;
+  });
+
+
+  return batch.commit();
+};
+
+export const readMatchups = ({ uid, groupId }) => {
+  const bracketRef = firestore.collection('matchup');
+  const queryRef = bracketRef.where('uid', '==', uid)
+                             .where('groupId', '==', groupId);
+
+  return queryRef.get().then((snapshot) => {
+    if (snapshot.size > 0) {
+      return snapshot.docs.map(doc => doc.data());
+    }
+  });
+}
