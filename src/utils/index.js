04e512781ca66d2c6936539bba9aec02fcb99749
Pauras Swami <pauras53@yahoo.com>
2018-04-09 22:11:24 -0700

    move to utils

---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+export const iconNBALink = tricode => `http://i.cdn.turner.com/nba/nba/assets/logos/teams/secondary/web/${tricode}.svg`
+
+export const roundNames = {
+  1: "First Round",
+  2: "Semi",
+  3: "Conference",
+  4: "Finals",
+};
