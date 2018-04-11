498dcd92d3a17ef3e46a379ef3701240a5c6eee4
Pauras Swami <pauras53@yahoo.com>
2018-04-10 22:25:18 -0700

    react-select styles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from './store';
 
+import 'react-select/dist/react-select.css';
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
