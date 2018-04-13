7d88f2b1ecae79bfd164275a604a1a2bf8a59c04
Pauras Swami <pauras53@yahoo.com>
2018-04-12 21:58:27 -0700

    temp home page

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Home = ({ children, appState }) => (
-  <div>
-    <button className="btn btn-primary">Create Group</button>
+  <div className="jumbotron">
+    <h1 className="display-4">Welcome!</h1>
+    <p className="lead">App is currently in beta...</p>
+    <hr className="my-4" />
+    <p className="lead">
+      <a className="btn btn-primary btn-lg" href="" role="button">Learn more</a>
+    </p>
   </div>
 );
 
