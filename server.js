12c9ad097090caa0c84a46cd13f70d32269c2ad1
Pauras Swami <pauras53@yahoo.com>
2018-04-13 23:35:05 -0700

    try with 'api'

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const request = require('request');
 const paths   = require('./config/paths');
 
-const app  = express();
-
 const NBA_BRACKETS_URL = 'https://data.nba.net/prod/v1/2017/playoffsBracket.json';
 const port = process.env.PORT || 5000;
 const host = process.env.HOST || '0.0.0.0';
+
+const app  = express();
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(paths.appBuild));
 }
 
-app.get('*/api/v1/brackets/nba', function (req, res) {
+app.get('/v1/brackets/nba', function (req, res) {
   request(NBA_BRACKETS_URL, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var info = JSON.parse(body)
