dbf314cd4eb369b4a0c5ceb035161a1144baa23f
Pauras Swami <pauras53@yahoo.com>
2018-04-21 23:56:07 -0700

    add current user to group on creation

--- my-app/my-app/src/resolvers/Mutation/group.js
+++ my-app/my-app/src/resolvers/Mutation/group.js
@@ -1,34 +1,40 @@
 const { getUserId } = require('../../utils')
 
 const group = {
-  async createGroup(parent, data, ctx, info) {
+  async createGroup(parent, { data }, ctx, info) {
     const userId = getUserId(ctx)
 
     if (userId) {
-      return ctx.db.mutation.createGroup({ data }, info)
-    }
-  },
-
-  async addUserToGroup(parent, { groupId, userId }, ctx, info) {
-    const groupExists = await ctx.db.exists.Group({ id: groupId })
-
-    if (!groupExists || !userId) {
-      throw new Error(`Group not found or you do not have access`)
-    }
-
-    return ctx.db.mutation.updateGroup(
-      {
-        where: { id: groupId },
+      return ctx.db.mutation.createGroup({
         data: {
+          ...data,
           users: {
             connect: {
               id: userId
             }
           }
         },
+      });
+    }
+  },
+
+  async addUserToGroup(parent, { groupId, userId }, ctx, info) {
+    const groupExists = await ctx.db.exists.Group({ id: groupId });
+
+    if (!groupExists || !userId) {
+      throw new Error(`Group not found or you do not have access`)
+    }
+
+    return ctx.db.mutation.updateGroup({
+      where: { id: groupId },
+      data: {
+        users: {
+          connect: {
+            id: userId
+          }
+        }
       },
-      info,
-    )
+    }, info);
   },
 
   // async deletePost(parent, { id }, ctx, info) {
