11113a002e0ebc14a486f65b1e504efd6092d26c
Pauras Swami <pauras53@yahoo.com>
2018-04-27 23:02:15 -0700

    create login and signup

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,46 @@ export default class Api {
     this.client = client;
   }
 
+  login = ({ email, password }) => {
+    return this.client.mutate({
+      mutation: gql`
+        mutation login($email: String!, $password: String!) {
+          login(email: $email, password: $password) {
+            token
+            user {
+              id
+              username
+              email
+            }
+          }
+        }
+      `,
+      variables: { email, password },
+    })
+  }
+
+  signup = ({ email, username, password }) => {
+    return this.client.mutate({
+      mutation: gql`
+        mutation signup(
+          $email: String!,
+          $username: String!
+          $password: String!,
+        ) {
+          login(email: $email, password: $password) {
+            token
+            user {
+              id
+              username
+              email
+            }
+          }
+        }
+      `,
+      variables: { email, username, password },
+    })
+  }
+
   getNBABracket() {
     return this.client.query({
       query: gql`
