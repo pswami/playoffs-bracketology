0a42b8be1fd45183e324066efc300b2abeb11e4d
pswami <pauras53@yahoo.com>
2022-04-11 23:03:23 -0700

    clean up

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { prisma } = require('./server/generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
 const resolvers = require('./server/resolvers')
 const paths   = require('./config/paths');
-const hsp   = require('heroku-self-ping')
 
 const server = new GraphQLServer({
   typeDefs: './server/schema.graphql',
@@ -28,7 +27,5 @@ const options = {
   subscriptions: '/subscriptions',
   playground: '/playground',
 };
-
-hsp.default("https://bracketology-beta.herokuapp.com/");
 
 server.start(options, (url) => console.log(`Server is running on`, url))
